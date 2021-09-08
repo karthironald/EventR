5ae28422ed1f0675b9b57f5bf328c6a306909a64
Karthick Selvaraj <karthick@mallow-tech.com>
2021-09-08 22:58:02 +0530

    Initial Commit

---
+++ EventR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EventR
+//
+//  Created by Karthick Selvaraj on 08/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EventR/EventRApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventRApp.swift
+//  EventR
+//
+//  Created by Karthick Selvaraj on 08/09/21.
+//
+
+import SwiftUI
+
+@main
+struct EventRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
