fca510ce4cf7fb2b8b32648df6801d09b4cd1384
Karthick Selvaraj <karthick@mallow-tech.com>
2021-09-09 07:17:59 +0530

    Event Image loading and cell configuration added

--- EventR/View/EventRowView.swift
+++ EventR/View/EventRowView.swift
@@ -12,16 +12,32 @@ struct EventRowView: View {
     var event: Event
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(event.title)
-                .bold()
-            Text(event.venue.displayLocation)
-                .font(.footnote)
-                .foregroundColor(.secondary)
-            Text(event.datetimeUTC)
-                .font(.footnote)
-                .foregroundColor(.secondary)
+        HStack {
+            AsyncImage(url: URL(string: event.performers.first?.images.huge ?? "")!) {
+                Text("Loading")
+            } image: {
+                Image(uiImage: $0)
+                    .resizable()
+            }
+            .frame(width: 70, height: 70, alignment: .center)
+            .clipShape(RoundedRectangle(cornerRadius: 5))
+            VStack(alignment: .leading) {
+                Text(event.title)
+                    .bold()
+                Text(event.venue.displayLocation)
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+                Text(event.datetimeUTC)
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+            }
+            Spacer()
         }
+        .padding(10)
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 5))
+        .shadow(color: .gray, radius: 3, x: 3, y: 3)
+        .frame(height: 100, alignment: .center)
     }
 }
 
